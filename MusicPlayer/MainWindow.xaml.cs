e40bf0b65e114b6164732a037f4a8c5f9f646094
ADILET <alpha.z91@mail.ru>
2019-06-07 17:28:46 +0600

    музплейер добавлен работает

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace MusicPlayer
@@ -11,56 +13,70 @@ namespace MusicPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        private bool userIsDraggingSlider = false;
         public MainWindow()
         {
             InitializeComponent();
-            
         }
-        
-        string[] files;
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        private void btnOpen_Click(object sender, RoutedEventArgs e)
+
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.AddExtension = true;
-            ofd.DefaultExt = "*.*";
-            ofd.Filter = "All files (*.*)|*.*";
-            ofd.Multiselect = true;
-            ofd.ShowDialog();
-
-            files = ofd.FileNames;
-
-            foreach (string song in files)
+            if (mediaPlayer.Source != null)
             {
-                if (!listBox.Items.Contains(song))
-                {
-                    listBox.Items.Add(song);
-                }
+                timeLabel.Content = String.Format("{0} / {1}",
+                    mediaPlayer.Position.ToString(@"mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"mm\:ss"));
+                progressSlider.Minimum = 0;
+                progressSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                progressSlider.Value = mediaPlayer.Position.TotalSeconds;
+            }
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                mediaPlayer.Open(new Uri(openFileDialog.FileName));
+                musicName.Content = Path.GetFileName(openFileDialog.FileName);
+                mediaPlayer.Play();
             }
 
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 1);
-            timer.Tick += timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
             timer.Start();
         }
 
-        private void timer_Tick(object sender, EventArgs e)
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
-
-        private void btnPlay_Click(object sender, RoutedEventArgs e)
-        {
-            lblName.Content = (listBox.SelectedValue).ToString();
-            mediaPlayer.Open(new Uri((listBox.SelectedValue).ToString()));
             mediaPlayer.Play();
         }
 
-        private void listBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
-            lblName.Content = (listBox.SelectedValue).ToString();
-            mediaPlayer.Open(new Uri((listBox.SelectedValue).ToString()));
-            mediaPlayer.Play();
+            mediaPlayer.Pause();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void ProgressSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void ProgressSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            mediaPlayer.Position = TimeSpan.FromSeconds(progressSlider.Value);
         }
     }
 }
+/*Создать приложение, которое позволяет проигрывать в фоне музыку (с использованием потока-демона), 
+а также вести текстовые записи.
+При закрытии приложения текстовые записи должны быть сохранены в отдельном потоке в файл 
+(не используйте асинхронность для наглядности примера).*/
